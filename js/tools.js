62173318bb143cda68310ba9e8e3098f75228bf2
rextao <87639243@qq.com>
2018-10-09 16:28:54 +0800

    1、新增tool.js用于存放工具函数 2、新增openMessage，信息提醒

---
+++ js/tools.js
@@ -0,0 +1,18 @@
+(function (w) {
+  if (!w.tools) {
+    return;
+  }
+  var tools = {};
+  tools.openMessage = function (msg, type) {
+    if (!msg) {
+      return;
+    }
+    var html = '<div class="rt-message ' + (type || 'success') + '" >' + msg + '</div>';
+    $('body').append(html);
+    setTimeout(function () {
+      $('.rt-message').remove();
+    }, 3000);
+  };
+
+  w.tools = tools;
+})(window);
