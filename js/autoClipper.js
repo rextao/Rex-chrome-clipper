8a3e7057147ac442e7f137c09907962e560a2de5
rextao <87639243@qq.com>
2018-10-09 22:22:43 +0800

    知乎专栏自动获取

--- js/autoClipper.js
+++ js/autoClipper.js
@@ -1,5 +1,46 @@
 var AutoClipper =function () {
+  var hostMap = {};
+  var preProcess = new PreProcess();
+  // 知乎专栏：http://zhuanlan.*.com/;
+  hostMap['zhuanlan'] = function () {
+    tools.openMessage('当前为知乎页面');
+    var $article = $('article');
+    var $header = $article.find('header');
+    var $headerNext = $header.next();
+    if(!$article && !$header && !$headerNext){
+      tools.openMessage('未成功剪辑知乎页面！！','error');
+      return;
+    }
+    $header.find('h1').addClass('rt-clipper-save-hook');
+    $headerNext.addClass('rt-clipper-save-hook');
+    $headerNext.nextAll().remove();
+    preProcess.init();
+  };
+
+  /**
+   * 将类似 zhihu_com 转换为 zhihu.com
+   * zhihu.com这样的字符串无法作为关键字
+   * @param key
+   */
+  function keyConvert(key) {
+    // 如不存在_不转换
+    if(key.indexOf('_') === -1){
+      return key;
+    }else {
+      key.replace(/_/g,'.')
+    }
+  }
+  function autoClipper() {
+    var hostname = document.location.hostname;
+    for(var key in hostMap){
+      key = keyConvert(key);
+      if(hostname.indexOf(key) !== -1){
+        hostMap[key]();
+        return;
+      }
+    }
+  }
   this.init = function () {
-    
+    autoClipper()
   }
 };
