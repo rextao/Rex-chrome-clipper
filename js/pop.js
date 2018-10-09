fa58584ef5820d8d288fde87142ea505e1d2588d
rextao <87639243@qq.com>
2018-10-09 16:03:27 +0800

    调整popup功能

--- js/pop.js
+++ js/pop.js
@@ -1,13 +1,31 @@
 (function(){
-  $('#btn').on('click',function () {
+  $('#startClipBtn').on('click',function () {
     chrome.tabs.query({active:true, currentWindow:true}, function (tab) {//获取当前tab
       //向tab发送请求
       chrome.tabs.sendMessage(tab[0].id, {
-        action: "send"
+        action: "startClip"
       }, function (response) {
-        console.log(response);
+      });
+    });
+  });
+  $('#destoryClipBtn').on('click',function () {
+    chrome.tabs.query({active:true, currentWindow:true}, function (tab) {//获取当前tab
+      //向tab发送请求
+      chrome.tabs.sendMessage(tab[0].id, {
+        action: "destoryClip"
+      }, function (response) {
+      });
+    });
+  });
+
+
+  $('#preProcessBtn').on('click',function () {
+    chrome.tabs.query({active:true, currentWindow:true}, function (tab) {//获取当前tab
+      //向tab发送请求
+      chrome.tabs.sendMessage(tab[0].id, {
+        action: "preProcess"
+      }, function (response) {
       });
     });
   })
-
 })();
