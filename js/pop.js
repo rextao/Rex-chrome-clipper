608611869620836e4fdcd8104e77e1863ff4ce34
rextao <87639243@qq.com>
2018-10-19 17:08:11 +0800

    增加badge提示，避免不知道运行

--- js/pop.js
+++ js/pop.js
@@ -1,5 +1,7 @@
 (function(){
   $('#popup').on('click',function (e) {
+    // 显示不了三个汉字
+    chrome.browserAction.setBadgeText({text: '汪...'});
     var id = e.target.id;
     // 如点击的不是button
     if(!id || id ==='popup'){
@@ -10,6 +12,10 @@
       chrome.tabs.sendMessage(tab[0].id, {
         action: id
       }, function (response) {
+        // 有返回时，3秒后将badge设置为空
+        setTimeout(function () {
+          chrome.browserAction.setBadgeText({text: ''});
+        },3000)
       });
     });
   });
